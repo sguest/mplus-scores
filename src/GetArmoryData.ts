83195b11fdbe2a5fdacce2065893ad48a996618f
sguest <spencer.b.guest@gmail.com>
2022-10-18 23:12:19 -0500

    Proxy for armory calls

--- src/GetArmoryData.ts
+++ src/GetArmoryData.ts
@@ -6,7 +6,7 @@ const cachedData: {[key: string]: MplusData} = {};
 export default async function GetArmoryData(characterId: CharacterId) {
     const key = `${characterId.name}_${characterId.realm}_${characterId.region}`;
     if(!cachedData[key]) {
-        const response = await fetch(`https://worldofwarcraft.com/en-us/character/${characterId.region.toLowerCase()}/${characterId.realm.toLowerCase()}/${characterId.name.toLowerCase()}/pve/mythic.json`);
+        const response = await fetch(`http://localhost:3000/en-us/character/${characterId.region.toLowerCase()}/${characterId.realm.toLowerCase()}/${characterId.name.toLowerCase()}/pve/mythic.json`);
         const data = await response.json() as MplusData;
         cachedData[key] = data
     }
